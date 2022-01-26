1f4fc4ed2f6fc2fc074b18973e94f34acab8cef9
Belen-Ledda <belenledda1@gmail.com>
2022-01-26 18:32:47 -0300

    se modifico nombre de componentes a NavBar para cumplir con desafio coder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Menu from './components/Menu';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Menu />
+      <NavBar />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import './menu.css';
+import './NavBar.css';
 
 
 
-export default function Menu() {
+export default function NavBar() {
     return ( 
         <ul>
             <li>Mi 1er item</li>
             <li>Mi 2do item</li>
+            <li>Mi 3er item</li>
             <li>Mi 3er item</li>
         </ul>
         );
